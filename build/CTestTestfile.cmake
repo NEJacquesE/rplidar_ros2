1a21ba65ba7ace8f9360878c340ddcc9c73514e6
NEJacquesE <nejacquese@gmail.com>
2025-06-13 18:14:24 -0700

    jeeny_bot_lidar

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeen/Projects/jeeny_ws/src/rplidar_ros2
+# Build directory: /home/jeen/Projects/jeeny_ws/src/rplidar_ros2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -10,10 +10,10 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
                 'serial_baudrate': 115200,  # A1 / A2
                 # 'serial_baudrate': 256000, # A3
-                'frame_id': 'laser',
+                'frame_id': 'laser_frame',
                 'inverted': False,
                 'angle_compensate': True,
             }],
